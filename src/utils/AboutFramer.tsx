ef13d88692aeda6c5cde31faf4c1b4a8b12f6f7e
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-10-31 10:01:55 -0300

    :art: About
    Ajuste no time animation css e AboutFramer

--- src/utils/AboutFramer.tsx
+++ src/utils/AboutFramer.tsx
@@ -13,7 +13,7 @@ function AboutFramer({ children } : any) {
         style={{
           transform: isInView ? "none" : "translateX(-200px)",
           opacity: isInView ? 1 : 0,
-          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 2s"
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 3s"
         }}
       >
         {children}
