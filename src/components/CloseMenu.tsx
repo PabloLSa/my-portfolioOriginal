090edab8900df437bb371862fed4094571f0287d
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-11-05 17:47:25 -0300

    :art: Menu
    Add FramerMotion

--- src/components/CloseMenu.tsx
+++ src/components/CloseMenu.tsx
@@ -1,14 +1,13 @@
+import FramerMotion from '@/utils/FramerMotion';
 import { PiArrowElbowRightUpLight } from 'react-icons/pi';
 const CloseMenu = () => {
 
 
   return (
+    <FramerMotion>
     <span className="hover:shadow-xl text-[#C850C0] group-hover:text-cyan-500 transition-all duration-100"><PiArrowElbowRightUpLight /></span>
-    
-    
-   
-
-   
+  
+   </FramerMotion>
   );
 }
 
