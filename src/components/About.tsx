e468a560307a904c62cd8536d41c2bd3f869f725
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-11-05 21:20:02 -0300

    :art: About | ABoutFramer
    AboutFramer voltando para o About só que com a lógica CSS do MenuMotion, porem no tempo extato que o About precisa

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ import themeContext from "@/context/themeContext";
 import { useContext, useState, useEffect } from "react";
 import AboutFramer from "@/utils/AboutFramer";
 import AnimatedSocial from "./AnimatadeSocial";
-import MenuMotion from "@/utils/MenuMotion";
+
 
 const About = () => {
   const theme = useContext(themeContext);
@@ -52,7 +52,7 @@ const About = () => {
 
         <div className="w-full md:w-1/2 text-left">
           <h1 className={`texto-digitado font-bold text-2xl m-5 mb-0 ${themeText}`}>Desenvolvedor Web</h1>
-          <MenuMotion>
+          <AboutFramer>
 
             <div className={`font-semibold text-xl m-5 ${themeText}`}> Campinas, São Paulo
 
@@ -66,7 +66,7 @@ const About = () => {
               Sou um desenvolvedor Full Stack, com paixão pelo design de  Front-end. Em agosto de 2022, realizei uma transição de carreira e me juntei à Trybe, onde através de projetos desafiadores e envolventes, tornei-me um especialista completo.
               Minha jornada nesse universo de programação tem sido incrível, hoje me sinto capaz de criar interfaces de usuário que são não apenas atraentes, mas também responsivas e funcionais!!
             </p>
-          </MenuMotion>
+          </AboutFramer>
         </div>
       </div>
     </>
