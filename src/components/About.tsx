84eac669c2d1560d107afa1a7c60f7daabd4131b
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-11-09 16:13:04 -0300

    :art: About
    Mudança para dowload cv direto do servidor

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -59,12 +59,16 @@ const About = () => {
 
             <div className={`font-semibold text-xl m-5 ${themeText}`}> Campinas, São Paulo
 
-              <a href="https://drive.google.com/uc?export=dowload&id=1NHm8AKSbOEY0u2pHXLCmWSd8TDX2kNnE" className={`group relative text-sm ml-3 hover:px-5 rounded-xl border p-2 border-solid border-purple-600 ${themeText}`}>
+              <button className={`group relative text-sm ml-3 hover:px-5 rounded-xl border p-2 border-solid border-purple-600 ${themeText}`}>
 
-                <div className="absolute inset-0 h-full scale-0 rounded-lg transition-all duration-700 group-hover:scale-75 group-hover:bg-[#C850C0]/40"></div>
-                
-                Dowload CV
-              </a>
+                <a
+                  href="/CV-Front-end.pdf"
+                  download="CV-Front-end.pdf"
+                  className="block h-full w-full"
+                >
+                  <div className="absolute inset-0 h-full scale-0 rounded-lg transition-all duration-700 group-hover:scale-75 group-hover:bg-[#C850C0]/40"></div>
+                  Download CV
+                </a>              </button>
             </div>
             <p className={`text-left flex flex-col font-semibold m-5 ${textlight}`}>
               Sou um desenvolvedor Full Stack, com paixão pelo design Front-end. Em agosto de 2022, realizei uma transição de carreira e me juntei à Trybe, onde através de projetos desafiadores e envolventes, tornei-me um especialista completo.
